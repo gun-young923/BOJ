9bb3a0eea9f3a855a2ae3f9e07abd5409ef7db8f
gyl923 <bajsy923@naver.com>
2021-10-10 21:45:03 +0900

    Create #2437.py

---
+++ #2437/#2437.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+w = list(map(int, sys.stdin.readline().split()))
+w.sort()
+cnt = 0
+for i in range(n):
+    if cnt+1 >= w[i]:
+        cnt += w[i]
+    else:
+        break
+print(cnt+1)
