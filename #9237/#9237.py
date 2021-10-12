a407763a0c50b5a8605223b0c7322e8d0300d5ec
gyl923 <bajsy923@naver.com>
2021-10-12 11:26:41 +0900

    Update #9237.py

--- #9237/#9237.py
+++ #9237/#9237.py
@@ -3,3 +3,13 @@ arr = sorted(list(map(int, input().split())),reverse=1)
 for i in range(n):
     arr[i]= arr[i]-n+i
 print(max(arr)+2+n)
+
+#1
+""" n = int(input())
+arr = list(map(int, input().split()))
+arr.sort(reverse=True)
+total = 1+arr[0]+1
+for i in range(1,n):
+    total = max(total,arr[i]+i+2)
+print(total) """
+
