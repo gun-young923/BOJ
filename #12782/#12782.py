97b5d4f9b49f6b71dc3330051a128d4953688932
gyl923 <bajsy923@naver.com>
2021-10-14 21:30:50 +0900

    Update #12782.py

--- #12782/#12782.py
+++ #12782/#12782.py
@@ -5,3 +5,13 @@ for _ in range(int(input())):
         if x!=y:
             arr.append([x,y])
     print(max(arr.count(['1','0']),arr.count(['0','1'])))
+
+#1
+""" for _ in range(int(input())):
+    cnt1=0
+    a,b = map(str,input().split())
+    cnt = abs(a.count('1')-b.count('1'))
+    for i in range(len(a)):
+        if a[i]!=b[i]:
+            cnt1+=1
+    print(cnt + (cnt1-cnt)//2) """
