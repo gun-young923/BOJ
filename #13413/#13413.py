6dfe3e1ba8d4a3462c47ebc636d30c211acf0932
gyl923 <bajsy923@naver.com>
2021-10-15 21:39:11 +0900

    Update #13413.py

--- #13413/#13413.py
+++ #13413/#13413.py
@@ -4,3 +4,11 @@ for t in range(int(I())):
     s=[v=='B' for v,w in zip(I(),I())if v!=w]
     v,w=sum(s),len(s)
     print(w-min(v,w-v))
+
+    """ for i in range(int(input())):
+    n = int(input())
+    arr1 = list(input())
+    arr2 = list(input())
+
+    diff = [(a,b) for a,b in zip(arr1,arr2) if a!=b]
+    print(max(diff.count(('W','B')),diff.count(('B','W')))) """
