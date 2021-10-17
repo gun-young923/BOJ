f642304bc0c3338a74ba65fe018645f3e54a9a0e
gyl923 <bajsy923@naver.com>
2021-10-17 21:10:03 +0900

    Update #13904.py

--- #13904/#13904.py
+++ #13904/#13904.py
@@ -19,3 +19,32 @@ for i in range(n):
             ans[arr[i][0]-1-j] = arr[i][1]
 print(sum(ans))
     
+
+#1
+""" import sys
+
+def f_max(s):
+    cnt=[0,0]
+    for i in range(len(s)):
+        if s[i][1] > cnt[1]:
+            cnt=s[i]
+    return cnt
+
+def f_day(s,x):
+    a=[]
+    for i in range(len(s)):
+        if s[i][0] >= x:
+            a.append(s[i])
+    return a
+
+input = sys.stdin.readline
+arr = [list(map(int, input().split())) for i in range(int(input()))]
+n = max(arr)[0]
+ans = [0]*n
+for i in range(n):
+    arr_1 = f_day(arr, n-i)
+    ans[n-i-1] = (f_max(arr_1)[1])
+    if arr_1==[]:
+        continue
+    arr.remove(f_max(arr_1))
+print(sum(ans)) """
