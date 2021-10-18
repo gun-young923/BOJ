24250848024af9c7b567d66cf66f7c9ec80c3f95
gyl923 <bajsy923@naver.com>
2021-10-18 23:58:25 +0900

    Update #15904.py

--- #15904/#15904.py
+++ #15904/#15904.py
@@ -9,3 +9,23 @@ except:
     print("I hate UCPC") 
     sys.exit()
 print("I love UCPC")
+
+
+#1
+""" arr = input()
+n=[]
+flag=0
+for i in "UCPC":
+    if arr.find(i)==-1:
+        flag=-1
+        break
+    else :
+        n.append(arr.find(i))
+        arr = list(arr)
+        arr = arr[arr.index(i)+1:]
+        arr = str(arr)
+    
+if flag==-1:
+    print("I hate UCPC")
+else :
+    print("I love UCPC") """
