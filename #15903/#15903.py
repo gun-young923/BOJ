69e2df479893e35fe2e3475f10d106ef8bfa61e7
gyl923 <bajsy923@naver.com>
2021-10-21 23:55:00 +0900

    Update #15903.py

--- #15903/#15903.py
+++ #15903/#15903.py
@@ -11,3 +11,14 @@ for _ in range(m):
     heapq.heappush(arr,ans)
     heapq.heappush(arr,ans)
 print(sum(arr))
+
+#1
+""" n,m = map(int,input().split())
+arr = list(map(int, input().split()))
+
+for _ in range(m):
+    arr.sort()
+    num = arr[0]+arr[1]
+    arr[0] = num
+    arr[1] = num
+print(sum(arr)) """
