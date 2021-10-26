b754780ee7bcdd48c63d1c0a4f119c86ba4c442b
gyl923 <bajsy923@naver.com>
2021-10-26 22:46:35 +0900

    Update #2217.py

--- #2217/#2217.py
+++ #2217/#2217.py
@@ -9,3 +9,20 @@ for i in range(n):
     if arr[i]*(i+1) > ans:
         ans = arr[i]*(i+1)
 print(ans)
+
+
+#2
+""" import sys
+In = sys.stdin.readline
+
+def main():
+    n = int(In())
+    rope = [0] * 10001
+    for _ in range(n):
+        rope[int(In())] += 1
+    m, s = 0, 0
+    for x in range(10000,-1,-1):
+        s += rope[x]
+        m = max(m, x * s)
+    print(m)
+main() """
