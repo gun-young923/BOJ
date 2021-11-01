d195b97f7dd8494daeb4b6f3f9b8544496f1f356
gyl923 <bajsy923@naver.com>
2021-11-01 17:20:47 +0900

    Update #1157_IM.py

--- #1157_IM/#1157_IM.py
+++ #1157_IM/#1157_IM.py
@@ -2,4 +2,9 @@ word = input().upper()
 arr=[]
 for i in range(26):
     arr.append(word.count(chr(i+65)))
+
 print('?'if arr.count(max(arr))>1 else chr(arr.index(max(arr))+65))    
+""" if arr.count(max(arr)) != 1:
+    print('?')
+else:    
+    print(chr(arr.index(max(arr)) + 65)) """
