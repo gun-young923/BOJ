3afbfa3e79739eb378f4c14d1ed19d97509f619d
gyl923 <bajsy923@naver.com>
2021-11-03 22:21:12 +0900

    Update #10250_IM.py

--- #10250_IM/#10250_IM.py
+++ #10250_IM/#10250_IM.py
@@ -1,4 +1,22 @@
+#1
+""" for _ in range(int(input())):    
+    f,r,n = map(int, input().split())
+    if n%f == 0:
+        a = str(f)
+        if n//f < 10:
+            b = '0'+str(n//f)
+        else:
+            b = str(n//f)
+    else:
+        a = str(n%f)
+        if n//f+1 < 10:
+            b = '0'+str(n//f+1)
+        else:
+            b = str(n//f+1)
+    print(a+b) """
+
 #2
 for _ in range(int(input())):    
     f,r,n = map(int, input().split())    
     print("%d%02d" % ((n-1)%f+1, (n-1)//f+1))
+
