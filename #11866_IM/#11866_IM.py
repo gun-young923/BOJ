ff0e5542b69cf12240cb7e374e64a37b271a1693
gyl923 <bajsy923@naver.com>
2021-11-05 22:12:25 +0900

    Create #11866_IM.py

---
+++ #11866_IM/#11866_IM.py
@@ -0,0 +1,17 @@
+#1
+import sys
+input = sys.stdin.readline
+
+n,k = map(int, input().split())
+arr = list(range(1,n+1))
+ans = []
+start = 0
+print('<',end='')
+for i in range(n):
+    start = (start+k-1)%len(arr)
+    ans.append(arr.pop(start))
+    if i == n-1:
+        print(ans[i],end='')
+    else :
+        print(ans[i],end=', ')
+print('>')
