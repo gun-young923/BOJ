c681af592e9c0ff744c687168fef35be555407a8
gyl923 <bajsy923@naver.com>
2021-11-05 22:13:01 +0900

    Update #11866_IM.py

--- #11866_IM/#11866_IM.py
+++ #11866_IM/#11866_IM.py
@@ -15,3 +15,18 @@ for i in range(n):
     else :
         print(ans[i],end=', ')
 print('>')
+
+#2    
+""" import sys
+input = sys.stdin.readline
+
+n,k = map(int, input().split())
+arr = list(range(1,n+1))
+ans = []
+start = 0
+for i in range(n):
+    start = (start + k-1)%len(arr)    
+    ans.append(str(arr.pop(start)))
+print('<'+', '.join(ans)+'>')
+ """
+
