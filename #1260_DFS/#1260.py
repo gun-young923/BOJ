17292b45ab3fb9b44046c6c99cda7228085888c5
gyl923 <bajsy923@naver.com>
2021-11-10 22:23:07 +0900

    Update #1260.py +1

--- #1260_DFS/#1260.py
+++ #1260_DFS/#1260.py
@@ -1,4 +1,4 @@
-import sys
+""" import sys
 from collections import deque
 input = sys.stdin.readline
 
@@ -7,9 +7,7 @@ line = []
 arr = [[0]*(n+1) for _ in range(n+1)]
 for i in range(l):
     a,b = map(int, input().split())
-    line.append([a,b])
-    arr[a][b] = 1
-    arr[b][a] = 1
+    arr[a][b] = arr[b][a] = 1
 
 def dfs(current_node, row, foot_print):
     foot_print += [current_node]
@@ -17,7 +15,7 @@ def dfs(current_node, row, foot_print):
         if row[current_node][search_node] and search_node not in foot_print:
             foot_print = dfs(search_node, row, foot_print)
     return foot_print
-    
+
 def bfs(start):
     queue = deque([start])
     footprint = [start]
@@ -31,4 +29,46 @@ def bfs(start):
     
 
 print(dfs(s,arr,[]))
-print(bfs(s))
+print(bfs(s)) """
+
+
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m,v = map(int, input().split())
+line = []
+arr = [[0]*(n+1) for _ in range(n+1)]
+for i in range(l):
+    a,b = map(int, input().split())
+    arr[a][b] = arr[b][a] = 1
+
+
+def dfs(v):
+    visited[v]=True
+    print(v,end=' ')
+    for i in range(1,n+1):
+        if not visited[i] and g[v][i]==1:
+            dfs(i)
+
+
+def bfs(v):
+    queue = deque([v])
+    visited[v] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v,end=' ')
+
+        for i in range(1,n+1):
+            if not visited[i] and g[v][i]==1:
+                queue.append(i)
+                visited[i]=True
+
+
+visited = [False]*(n+1)
+dfs(v)
+print()
+visited = [False]*(n+1)
+bfs(v)
