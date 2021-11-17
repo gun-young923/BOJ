45fc468f252852622e93076688e11689a989d7f1
gyl923 <bajsy923@naver.com>
2021-11-17 20:28:32 +0900

    Edit #2210_dfs.py

--- #2210_dfs/#2210_dfs.py
+++ #2210_dfs/#2210_dfs.py
@@ -1,23 +1,18 @@
-def dfs(x, y, number):
-    if len(number) == 6: 
-        if number not in result: 
-            result.append(number)
+def dfs(x, y, word):
+    if len(word) == 6: 
+        if word not in ans: 
+            ans.append(word)
         return
-        
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1] 
     for k in range(4):
-        ddx = x + dx[k]
-        ddy = y + dy[k]
-        
-        if 0 <= ddx < 5 and 0 <= ddy < 5:
-            dfs(ddx, ddy, number + matrix[ddx][ddy]) 
-
-
-matrix = [list(map(str, input().split())) for _ in range(5)]
-
-result = []
+        X = x + dx[k]
+        Y = y + dy[k]
+        if 0 <= X < 5 and 0 <= Y < 5:
+            dfs(X, Y, word + g[X][Y]) 
+g = [list(map(str, input().split())) for _ in range(5)]
+ans = []
 for i in range(5):
     for j in range(5):
-        dfs(i, j, matrix[i][j]) 
-print(len(result))
+        dfs(i, j, g[i][j]) 
+print(len(ans))
