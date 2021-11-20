df383cfe96a5ef960064b0e0f43ce4f930a00a83
gyl923 <bajsy923@naver.com>
2021-11-20 17:03:05 +0900

    Edit #2210_dfs.py

--- DFS_BFS/#2210_dfs.py
+++ DFS_BFS/#2210_dfs.py
@@ -1,4 +1,4 @@
-def dfs(x, y, word):
+""" def dfs(x, y, word):
     if len(word) == 6: 
         if word not in ans: 
             ans.append(word)
@@ -15,4 +15,27 @@ ans = []
 for i in range(5):
     for j in range(5):
         dfs(i, j, g[i][j]) 
+print(len(ans))
+ """
+
+# 1
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+
+def dfs(x,y,word):
+    if len(word) == 6:
+        ans.add(word)                   # add()
+        return
+    for i in range(4):
+        X = x + dir[i][0]
+        Y = y + dir[i][1]
+        if 0 <= X < 5 and 0 <= Y < 5:
+            dfs(X,Y,word+g[X][Y])
+ans = set([])                           # set([])
+dir = [(-1,0),(1,0),(0,-1),(0,1)]
+g = [input().split() for _ in range(5)]
+for i in range(5):
+    for j in range(5):
+        dfs(i,j,g[i][j])
 print(len(ans))
