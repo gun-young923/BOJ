be77e5a491829987579b0e34f266a8e9121d9146
gyl923 <bajsy923@naver.com>
2021-11-21 23:43:02 +0900

    Add bfs2 in #21736_bfs.py

--- DFS_BFS/#21736_bfs.py
+++ DFS_BFS/#21736_bfs.py
@@ -1,7 +1,5 @@
-# bfs 로 풀기
-
-# dfs visited[] 사용
-import sys
+# bfs1 visited[] 사용
+""" import sys
 from collections import deque
 input =sys.stdin.readline
 
@@ -33,34 +31,39 @@ for i in range(n):
         start = (i,info.index('I'))
     cmap.append(info)
 ans = bfs(start)
-print(ans if ans != 0 else 'TT')
+print(ans if ans != 0 else 'TT') """
 
-# 지나간 곳 'X'로 교체
-""" import sys
-sys.setrecursionlimit(1000000)
+# bfs2 지나간 곳 'X'로 교체
+import sys
+from collections import deque
 input =sys.stdin.readline
 
-def dfs(x,y):       
-    global cnt 
-    for i in range(4):
-        X = x + dir[i][0]
-        Y = y + dir[i][1]
-        if 0 <= X < n and 0 <= Y < m and cmap[X][Y] != 'X':
-            if cmap[X][Y] == 'P':
-                cnt += 1        
-            cmap[X][Y] = 'X'
-            dfs(X,Y)
+def bfs(s):
+    global cnt
+    x,y = s
+    queue = deque()
+    queue.append(s)
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            X = x + dir[i][0]
+            Y = y + dir[i][1]
+            if 0 <= X < n and 0 <= Y < m and cmap[X][Y] != 'X':
+                queue.append((X,Y))
+                if cmap[X][Y] == 'P':
+                    cnt += 1
+                cmap[X][Y] = 'X'
     return cnt
 cmap = []
 cnt = 0
 dir = [(-1,0),(1,0),(0,-1),(0,1)]
 n, m = map(int, input().split())
+visited = [[0]*m for _ in range(n)]
 for i in range(n):
     info = list(input().rstrip())
     if 'I' in info:
-        I = info.index('I')
-        start = (i,I)
-        info[I] = 'X'
+        start = (i,info.index('I'))
+        info[info.index('I')] = 'X'
     cmap.append(info)
-ans = dfs(start[0],start[1])
-print(ans if ans != 0 else 'TT') """
+ans = bfs(start)
+print(ans if ans != 0 else 'TT')
