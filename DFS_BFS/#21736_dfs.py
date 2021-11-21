515dd205a9aaf329ace296874dbd2342c406a908
gyl923 <bajsy923@naver.com>
2021-11-21 19:30:58 +0900

    Create #21736_dfs.py

---
+++ DFS_BFS/#21736_dfs.py
@@ -0,0 +1,28 @@
+# dfs visited[] 사용
+import sys
+sys.setrecursionlimit(1000000)
+input =sys.stdin.readline
+def dfs(x,y):       
+    global cnt
+    for i in range(4):
+        X = x + dir[i][0]
+        Y = y + dir[i][1]
+        if 0 <= X < n and 0 <= Y < m and cmap[X][Y] != 'X' and visited[X][Y] == 0:
+            visited[X][Y] = 1
+            if cmap[X][Y] == 'P':
+                cnt += 1        
+            dfs(X,Y)
+    return cnt
+cmap = []
+cnt = 0
+dir = [(-1,0),(1,0),(0,-1),(0,1)]
+n, m = map(int, input().split())
+visited = [[0]*m for _ in range(n)]
+
+for i in range(n):
+    info = list(input().rstrip())
+    if 'I' in info:
+        start = (i,info.index('I'))
+    cmap.append(info)
+ans = dfs(start[0],start[1])
+print(ans if ans != 0 else 'TT')
