8d3dcad97e9c535768d9a08e4b0261a710495fd4
gyl923 <bajsy923@naver.com>
2021-12-01 23:06:58 +0900

    Edit #17086.py

--- DFS_BFS/#17086.py
+++ DFS_BFS/#17086.py
@@ -2,3 +2,6 @@ import sys
 from collections import deque
 sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
+
+n,m = map(int,input())
+arr = [list(map(int, input().split())) for _ in range(n)]
