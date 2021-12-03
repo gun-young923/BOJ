4149ebe1bb17d8fc5234d273ff1d97416c4bb4e2
gyl923 <bajsy923@naver.com>
2021-12-03 18:25:47 +0900

    Edit #17086_bfs.py

--- DFS_BFS/#17086_bfs.py
+++ DFS_BFS/#17086_bfs.py
@@ -72,4 +72,34 @@ for i in range(n):
         ans.append(bfs(i,j,-1))
 print(max(ans)) """
 
+# 1의 위치 찾아서 1부터 0으로 count 하며 뻗어나가며 0 에서 가장 가까운 1까지의 거리 표기
+import sys
+from collections import deque
+input = sys.stdin.readline
 
+def bfs(x,y,cnt):
+    ans = 0
+    queue = deque()
+    queue.extend(temp)
+    while queue:
+        x,y,cnt = queue.popleft()
+        ans = max(ans,cnt)
+        for i in range(8):
+            X = x + dir[i][0]
+            Y = y + dir[i][1]
+            if 0 <= X < n and 0 <= Y < m and arr[X][Y]==0:
+                queue.append((X,Y,cnt+1))
+                arr[X][Y] = cnt + 1
+    return ans
+
+n, m=map(int, input().split())
+temp = []
+arr = []
+dir = [(1,0),(1,1),(1,-1),(-1,0),(-1,1),(-1,-1),(0,1),(0,-1)]
+for i in range(n):                              
+    line = list(map(int, input().split()))
+    arr.append(line)
+    for j in range(m):
+        if line[j] == 1:
+            temp.append((i,j,0))
+print(bfs(0,0,0))
