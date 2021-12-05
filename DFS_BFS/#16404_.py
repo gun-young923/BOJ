12e78b9348035144082b6d5ae7216489704447bf
gyl923 <bajsy923@naver.com>
2021-12-05 21:25:58 +0900

    Edit #16404_.py

--- DFS_BFS/#16404_.py
+++ DFS_BFS/#16404_.py
@@ -41,24 +41,22 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-n,m = map(int, input().split())         # 직원수 (승범포함)
-line = list(map(int, input().split()))  # 사수
-
-stat = dict()
-for i in range(1,n+1):  # 1~n 까지
-    stat[i] = 0
-link = [[] for _ in range(n+1)]
-for i in range(1,n):                    # link[사수인덱스] 에 부사수번호 append
-    link[line[i]].append(i+1)
-# print(link)
-
 def dfs(x,cnt):
     stat[x] += cnt
     for j in link[x]:   # 부사수들
-        if j == 0:
-            return
+        """ if j == 0:
+            return """
         dfs(j,cnt)
     return
+
+n,m = map(int, input().split())         # 직원수 (승범포함)
+line = list(map(int, input().split()))  # 사수
+stat = dict()
+for i in range(1,n+1):  # 1~n 까지 잔액 0 초기화
+    stat[i] = 0
+link = [[] for _ in range(n+1)]
+for i in range(1,n):                    # link[사수번호] 에 부사수번호 append
+    link[line[i]].append(i+1)
 
 for i in range(m):
     temp = list(map(int, input().split()))
