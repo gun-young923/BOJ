6bfdd3582bb3b3488cd5d56017ca2fa43a146708
gyl923 <bajsy923@naver.com>
2022-01-06 16:44:26 +0900

    Edit #1932.py

--- DynamicProgramming/#1932.py
+++ DynamicProgramming/#1932.py
@@ -39,11 +39,15 @@ input = sys.stdin.readline
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-ans = [[0]*i for i in range(1,n+1)]
-for i in range(1,n):
+# ans = [[0]*i for i in range(1,n+1)]
+""" for i in range(1,n):
     ans[i][0] = ans[i-1][0] + arr[i][0]
     ans[i][-1] = ans[i-1][-1] + arr[i][-1]
     for j in range(1,len(arr[i])-1):
-        ans[i][j] = max(ans[i-1][j-1]+arr[i][j], ans[i-1][j]+arr[i][j])
-
+        ans[i][j] = max(ans[i-1][j-1]+arr[i][j], ans[i-1][j]+arr[i][j]) """
+ans = []       
+for i in range(n):
+    ans += [0]
+    for j,a,b in zip(range(len(arr[i])),[0]+ans,ans+[0]):
+        ans[j] = max(a+arr[i][j], b+arr[i][j])
 print(max(ans))
